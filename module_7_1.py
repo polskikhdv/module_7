4c7e527d369aed264694c3bcb8f62cb349c98614
polskikhdv <polskikhdv@icloud.com>
2025-01-21 23:44:38 +0500

    Проблема с методом add добавляет дубли

--- module_7_1.py
+++ module_7_1.py
@@ -1,5 +1,4 @@
-
-__file_name = 'products.txt'
+from tkinter.font import names
 
 
 class Product:
@@ -9,65 +8,34 @@ class Product:
         self.category = category
 
     def __str__(self):
-        return (f'{self.name}, {self.weight}, {self.category}')
+        return f"{self.name}, {self.weight}, {self.category}"
 
-class Shop():
+class Shop:
     def __init__(self):
         self.__file_name = 'products.txt'
-
+        self._products = set()
 
     def get_products(self):
-        try:
-            with open(self.__file_name, 'r') as file:
-                return file.read()
-        except FileNotFoundError:
-            return ""
-
-
+        with open(self.__file_name, 'r', encoding='utf-8') as file:
+            return file.read()
 
     def add(self, *products):
-        current_products = {}
-        for line in self.get_products().splitlines():
-            name, weight, category = line.split(', ')
-            current_products[(name, category)] = float(weight)
-
         for product in products:
-            key = (product.name, product.category)
-            if key in current_products:
-                current_products[key] += product.weight
-                print(f'Продукт {product.name} уже был в магазине, его общий вес теперь равен {current_products[key]}.')
+            if product not in self._products:
+                self._products.add(product)
+                with open(self.__file_name, 'a', encoding='utf-8') as file:
+                    file.write(str(product) + '\n')
             else:
-                current_products[key] = product.weight
-                print(f"Продукт {product.name} добавлен в магазин.")
-        with open(self.__file_name, 'w') as file:
-            for (name, category), weight in current_products.items():
-                file.write(f"{name}, {weight}, {category}\n")
+                print("Продукт", product.name, "уже есть в магазине")
+
+# Пример использования классов
+shop = Shop()
+p1 = Product("Potato", 50.5, "Vegetables")
+p2 = Product("Spaghetti", 3.4, "Groceries")
+p3 = Product("Potato", 5.5, "Vegetables")
+p4 = Product('potato', 12, "veg")
 
 
+shop.add(p1)
 
-s1 = Shop()
-
-p1 = Product('Potato', 50.5, 'Vegetables')
-
-p2 = Product('Spaghetti', 3.4, 'Groceries')
-
-p3 = Product('Potato', 5.5, 'Vegetables')
-
-
-
-print(p2) # __str__
-
-
-
-s1.add(p1, p2, p3)
-
-
-
-print(s1.get_products())
-
-
-
-
-
-
-
+print(shop.get_products())
